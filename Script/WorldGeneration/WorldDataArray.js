5afc7874a4ab272582efe437acbd2f16261889ab
Guilherme Rossato <guilhermrossato@gmail.com>
2017-04-29 07:14:58 -0300

    Added Encoding on World Data
    And aligned bit packets to center of Uint32

--- Script/WorldGeneration/WorldDataArray.js
+++ Script/WorldGeneration/WorldDataArray.js
@@ -7,60 +7,84 @@
  */
 
 function WorldDataArray(sizeX, sizeY, sizeZ) {
-	let main = new Uint32Array(sizeX*sizeY*sizeZ/10);
+	let main = new Uint32Array(Math.ceil(sizeX*sizeY*sizeZ/10));
 	function flatten(x, y, z) {
 		return x+sizeX*(z+y*sizeZ);
 		//return x+z*sizeX+y*sizeX*sizeZ;
 	}
 	function getMask(area) {
+		//generated using: ("if (area === 0)\n\treturn 0x")+[0,1,2,3,4,5,6,7,8,9].reverse().map(i=>parseInt("1".repeat(3*i)+"10001"+"1".repeat(8*3-i*3+3),2).toString(16)).reduce((past, next, i)=>(`${past};\nelse if (area === ${(i)})\n\treturn 0x${next}`))+";";
 		if (area === 0)
-			mask = 0xfffffff8;
+			return 0xfffffff1;
 		else if (area === 1)
-			mask = 0xffffffc7;
+			return 0xffffff8f;
 		else if (area === 2)
-			mask = 0xfffffe3f;
+			return 0xfffffc7f;
 		else if (area === 3)
-			mask = 0xfffff1ff;
+			return 0xffffe3ff;
 		else if (area === 4)
-			mask = 0xffff8fff;
+			return 0xffff1fff;
 		else if (area === 5)
-			mask = 0xfffc7fff;
+			return 0xfff8ffff;
 		else if (area === 6)
-			mask = 0xffe3ffff;
+			return 0xffc7ffff;
 		else if (area === 7)
-			mask = 0xff1fffff;
+			return 0xfe3fffff;
 		else if (area === 8)
-			mask = 0xf8ffffff;
-		else // (area === 9)
-			mask = 0xc7ffffff;
-	}
-	this.get = function(x, y, z) {
-		if (x >= 0 && y >= 0 && z >= 0 && x < sizeX && y < sizeY && z < sizeZ) {
-			return this._get(x, y, z);
-		}
+			return 0xf1ffffff;
+		else if (area === 9)
+			return 0x8fffffff;
 	}
 	this._get = function(x, y, z) {
 		let index = flatten(x, y, z),
 			compactId = index / 10 | 0,
 			area = index % 10,
 			mask = getMask(area) ^ 0xffffffff;
-		return ((main[compactId] & mask) >> (area * 3));
-	}
-	this.set = function(x, y, z, value) {
-		if (x >= 0 && y >= 0 && z >= 0 && value >= 0 && x < sizeX && y < sizeY && z < sizeZ && value < 8) {
-			return this._set(x, y, z, value);
-		}
-		console.warn("Unnacepted parameters: ", {x:x, y:y, z:z, value:value, sizeX:sizeX, sizeY:sizeY, sizeZ:sizeZ});
+		//console.log(`index: ${index}, compactId: ${compactId}, area: ${area}, mask:${minL((mask.toString(2)), "0", 8*4, true,4)}`);
+		return ((main[compactId] & mask) >> (area * 3+1));
 	}
 	this._set = function(x, y, z, value) {
 		let index = flatten(x, y, z),
 			compactId = index / 10 | 0,
 			area = index % 10,
 			mask = getMask(area);
-		return (main[compactId] = (main[compactId] & mask) | (value << (area*3)));
+		//console.log(`index: ${index}, compactId: ${compactId}, area: ${area}, mask:${minL((mask.toString(2)), "0", 8*4, true,4)}`);
+		return (main[compactId] = (main[compactId] & mask) | (value << (area*3+1)+1));
 	}
-	this.toString = function() {
-		return "ohaio";
+	this.get = function(x, y, z) {
+		if (x >= 0 && y >= 0 && z >= 0 && x < sizeX && y < sizeY && z < sizeZ) {
+			return this._get(x, y, z);
+		}
+	}
+	this.set = function(x, y, z, value) {
+		if (x >= 0 && y >= 0 && z >= 0 && value >= 0 && x < sizeX && y < sizeY && z < sizeZ && value < 8) {
+			return this._set(x, y, z, value);
+		}
+		console.warn("Unnaccepted parameters: ", {x:x, y:y, z:z, value:value, sizeX:sizeX, sizeY:sizeY, sizeZ:sizeZ});
+	}
+	this.toArray = function() {
+		return new Array(...main);
+	}
+	this.encode = function() {
+		var chars = [];
+		for (let i = 0; i < main.length; i++) {
+			chars.push(	String.fromCharCode((main[i] & 0xFF000000) >>> 8*3),
+						String.fromCharCode((main[i] & 0x00FF0000) >>> 8*2),
+						String.fromCharCode((main[i] & 0x0000FF00) >>> 8*1),
+						String.fromCharCode((main[i] & 0x000000FF)));
+		}
+		return (chars.join(''));
+	}
+	this.decode = function(encoded) {
+		var chars = (encoded).split("").map(c => c.charCodeAt(0));
+		if (chars.length%4!==0)
+			return new Error("Unreadable: input length is not divisible by 4");
+		if (main.length*4 != chars.length)
+			return new Error("Unreadable: This instance size doesn't match the input");
+		for (let i = 0; i < main.length; i++) {
+			main[i] = (encoded.charCodeAt(i*4) << 8*3)+(encoded.charCodeAt(i*4+1) << 8*2)+(encoded.charCodeAt(i*4+2) << 8) + encoded.charCodeAt(i*4+3);
+		}
+		return true;
 	}
 }
 
@@ -69,17 +93,29 @@ WorldDataArray.prototype = {
 }
 
 WorldDataArray.test = function() {
-	var a = new WorldDataArray(16,15,13);
-	for (let x = 0 ; x < 16; x ++) {
-		for (let y = 0 ; y < 15; y ++) {
-			for (let z = 0 ; z < 13; z ++) {
+	var sx = 2;
+	var sy = 11;
+	var sz = 14;
+	var a = new WorldDataArray(sx,sy,sz);
+	var failCount = 0;
+	for (let x = 0 ; x < sx; x ++) {
+		for (let y = 0 ; y < sy; y ++) {
+			for (let z = 0 ; z < sz; z ++) {
 				for (let k = 0; k < 8; k++) {
 					a.set(x, y, z, k);
-					if (a.get(x,y,z) != k)
-						console.log(x, y, z);
+					if (a.get(x,y,z) != k) {
+						failCount++
+						if (failCount < 20)
+							console.warn("Setting",x, y, z,"wasn't sucessfull for",k);
+					}
 				}
 			}
 		}
+	}
+	if (failCount > 0) {
+		console.log("Failed "+failCount+" times out of "+sx*sy*sz*8+" = "+((((100000*failCount)/(sx*sy*sz*8))|0))/1000+"% failure")
+	} else {
+		console.log("Sucessful test");
 	}
 }
 
