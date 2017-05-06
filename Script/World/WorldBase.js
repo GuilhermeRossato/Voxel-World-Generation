ce975045d35a82b36744db29bd231982df4611ba
Guilherme Rossato <guilhermrossato@gmail.com>
2017-05-06 04:47:51 -0300

    World Basic Global Variables
    Both workers and the main application uses these values

---
+++ Script/World/WorldBase.js
@@ -0,0 +1,25 @@
+var blockTypes = {
+	undefined: 0,
+	air: 1,
+	dirt: 2,
+	grass: 3,
+	stone: 4,
+	log: 5,
+	leaves: 6,
+	flower: 7
+}
+
+const chunkSize = 24;
+const maxLoadedChunks = 72;
+
+function encodeWorldMessage(world) {
+	return ("c"+world.getCount()+","+world.encode());
+}
+
+function decodeWorldMessage(text) {
+	let nextComma, values = {};
+	nextComma = text.indexOf(",");
+	values.start = nextComma;
+	values.count = parseInt(text.substr(1, nextComma-1));
+	return values;
+}
