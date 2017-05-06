d473c64677f003d82ae109bd11e6facb5dfaf364
Guilherme Rossato <guilhermrossato@gmail.com>
2017-05-06 04:48:56 -0300

    Mobile support tests

--- Script/setup.js
+++ Script/setup.js
@@ -1,15 +1,10 @@
 var app, worker;
 
-function getWorker() {
-	try {
-		var w = new Worker("Script/WorldGeneration/WorldWorker1.js")
-		return w;
-	} catch(e) {
-		return FakeWorldGen;
-	}
-}
-
 window.addEventListener("load", () => {
 	app = Application;
 	app.init();
+});
+
+document.addEventListener("touchmove",function(event){
+	event.preventDefault();
 });
